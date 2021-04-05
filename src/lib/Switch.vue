d102e6cc1fc5295e2a3d1929ee3391f0edbabda6
Nightlue2 <basicperfect@outlook.com>
2021-04-05 17:26:31 +0800

    完成switch的loading部分

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,6 +1,8 @@
 <template>
   <button class="cloud-switch" @click="toggle" :class="{'cloud-switch-checked':value,'cloud-switch-disabled':disabled || loading,'cloud-switch-small':size,'cloud-switch-loading':loading}">
-    <span></span>
+    <div class="cloud-switch-loading-container">
+      <div class="cloud-switch-loading-content"></div>
+    </div>
   </button>
 </template>
 <script lang="ts">
@@ -35,50 +37,37 @@ $switch-unchecked-color:#bfbfbf;
   &:hover{
       cursor: pointer;
   }
-  > span {
+  > .cloud-switch-loading-container {
     position: absolute; top: 2px; left: 2px; height: $h2; width: $h2; background: white; border-radius: $h2 / 2; transition: all 350ms ease;
   }
   &-checked { background: $switch-checked-color;
-    > span { left: calc(100% - #{$h2} - 2px); }
+    > .cloud-switch-loading-container { left: calc(100% - #{$h2} - 2px); }
   }
   &-checked.cloud-switch-small{
-      >span{
+      >.cloud-switch-loading-container{
           left:calc(100% - #{$switch-small-h2} - 2px);
       }
   }
   &:focus { outline: none; }
   &:not(.cloud-switch-disabled):active {
-    > span { width: $h2 + 4px; }
+    > .cloud-switch-loading-container { width: $h2 + 4px; }
   }
   &:not(.cloud-switch-disabled):active.cloud-switch-checked {
-    > span { width: $h2 + 4px; margin-left: -4px; }
+    > .cloud-switch-loading-container { width: $h2 + 4px; margin-left: -4px; }
   }
   &-small{
       height: $switch-small-h; width: $switch-small-h * 2; border-radius: $switch-small-h / 2;
-      >span{
+      >.cloud-switch-loading-container{
           height: $switch-small-h2; width: $switch-small-h2; border-radius: $switch-small-h2 / 2; left:2px;top:2px;
-          &::before{
-            content:'';
-            width:67%;
-            height: 67%;
-            display: block;
-            border: 2px solid $switch-checked-color;
-            border-color:$switch-checked-color transparent transparent transparent;
-            border-radius: 100%;
-            position:relative;
-            transform: rotate(45deg);
-            left:6%;
-            top:3%;
-          }
       }
   }
   &:not(.cloud-switch-disabled):active.cloud-switch-small{//小按钮-未选中-点击状态
-      >span{
+      >.cloud-switch-loading-container{
           width: $switch-small-h2 + 2px;
       }
   }
   &:not(.cloud-switch-disabled):active.cloud-switch-checked.cloud-switch-small{//小按钮-选中-点击状态
-      >span{
+      >.cloud-switch-loading-container{
            width: $switch-small-h2 + 2px; margin-left: -2px; 
       }
   }
@@ -88,28 +77,58 @@ $switch-unchecked-color:#bfbfbf;
       &:hover,&:focus{
           cursor:not-allowed;
       }
-      >span{
+      >.cloud-switch-loading-container{
           cursor:inherit;
       }
   }
   &-loading{
-    >span{
-          cursor:inherit;
-          &::before{
-            content:'';
-            width:12px;
-            height: 12px;
-            display: block;
+    >.cloud-switch-loading-container{
+        cursor:inherit;
+        transform:rotate(45deg);
+        > .cloud-switch-loading-content{
+          width:14px;
+          height: 14px;
+          border: 2px solid $switch-checked-color;
+          border-color:$switch-checked-color transparent transparent transparent;
+          border-radius: 50%;
+          position:absolute;
+          left:0;
+          top:0;
+          right:0;
+          bottom:0;
+          margin:auto;
+          cursor:not-allowed; 
+          animation:loading 1s linear infinite;
+        }  
+    }
+  }
+  &-loading.cloud-switch-small{
+    >.cloud-switch-loading-container{
+        cursor:inherit;
+        transform:rotate(45deg);
+      > .cloud-switch-loading-content{
+            width:10px;
+            height: 10px;
             border: 2px solid $switch-checked-color;
             border-color:$switch-checked-color transparent transparent transparent;
-            border-radius: 100%;
-            position:relative;
-            transform: rotate(45deg);
-            left:6%;
-            top:3%;
-            cursor:not-allowed;         
-          }  
+            border-radius: 50%;
+            position:absolute;
+            left:0;
+            top:0;
+            right:0;
+            bottom:0;
+            margin:auto;
+            animation:loading 1s linear infinite;
       }
+    }
+  }
+}
+@keyframes loading {
+  0%{
+    transform:rotate(0deg);
+  }
+  100%{
+    transform: rotate(360deg);
   }
 }
 </style>
